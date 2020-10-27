37f0454c8f796716dab59a5ef6c77f49bfb03d69
jessemcardle <phoenix12544@gmail.com>
2020-10-27 21:51:29 +1100

    More syntax errors fixed

--- main.py
+++ main.py
@@ -89,7 +89,10 @@ while True:
             print ('Error')
         # DHT22 requires 2 seconds to give a reading, so make sure to add delay of above 2 seconds.
         sleep(300)
-    except:
-        raise Exception()
-        destroy()
-        break
+    
+
+    except:      
+            destroy()
+            raise Exception()
+            break
+        
